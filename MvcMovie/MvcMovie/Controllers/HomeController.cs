8ae7c6131a1fc972321aeebef77bb5fb080f2a23
jcqnly <jcqnly@gmail.com>
2018-06-21 00:19:02 -0700

    Add css styling

--- MvcMovie/MvcMovie/Controllers/HomeController.cs
+++ MvcMovie/MvcMovie/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace MvcMovie.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "tHiS iS oNE oWESome APp.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Don't CONTACT ME.";
 
             return View();
         }
